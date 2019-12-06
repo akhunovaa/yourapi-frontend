dd60bfeb8654da648f145f563cdb5575133782ee
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-06 17:39:21 +0300

    [NF] profile data added

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -3,6 +3,7 @@ import './Profile.css';
 import {NavLink} from "react-router-dom";
 import {Breadcrumb, Icon, Image, Segment} from "semantic-ui-react";
 import {loadUser} from "../util/APIUtils";
+import profile from '../img/profile.png';
 
 class Profile extends Component {
 
@@ -11,17 +12,21 @@ class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: null
+            user: {
+                imageUrl: 'https://yourapi.ru/individual/image/4',
+                name: 'Azat',
+                surname: 'Akhunov',
+                patrName: 'Akhmatovich'
+            }
         };
         this.loadUser = this.loadUser.bind(this);
         this.reload = this.reload.bind(this);
     }
 
-
     componentDidMount () {
         this._isMounted = true;
         const { handle } = this.props.match.params;
-        this.setState({user: this.props.currentUser})
+        //this.setState({user: this.props.currentUser})
         // this.loadUser(handle);
     }
 
@@ -69,7 +74,31 @@ class Profile extends Component {
                         </Breadcrumb>
                     </div>
                     <div className="profile-form-container">
+                        <div className="profile-avatar-container">
+                            <div className="profile-avatar">
+                                {
+                                    this.state.user.imageUrl ? (
+                                        <Image src={this.state.user.imageUrl} size='medium' circular verticalAlign='top' alt={this.state.user.name}/>
+                                    ) : (
+                                        <div className="text-avatar">
+                                            <span>{this.state.user.name && this.state.user.name[0]}</span>
+                                        </div>
+                                    )
+                                }
+                                <div className="profile-avatar-footer">
+                                    <Icon link name='photo' size={'large'} color={'grey'} />
+                                </div>
+                            </div>
+                            <div className="user-name-container">
+                                <span style={{paddingRight: '8px'}}>{this.state.user.surname}</span>
+                                <span style={{paddingRight: '8px'}}>{this.state.user.name}</span>
+                                <span style={{paddingRight: '8px'}}>{this.state.user.patrName}</span>
+                            </div>
+                            <div className="user-custom-icon-container">
+                                <Icon link name='cog' size={'large'} color={'grey'} />
+                            </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
