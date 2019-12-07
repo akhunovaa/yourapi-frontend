f42eab3832eab35841a0d6aa9c135145a6b581e9
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-07 14:46:47 +0300

    [NF] city data added

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import './Profile.css';
 import {NavLink} from "react-router-dom";
-import {Breadcrumb, Icon, Image, Input, Dropdown} from "semantic-ui-react";
+import {Breadcrumb, Dropdown, Icon, Image, Input} from "semantic-ui-react";
 import {loadUser} from "../util/APIUtils";
-import profile from '../img/profile.png';
 
 class Profile extends Component {
 
@@ -18,28 +17,29 @@ class Profile extends Component {
                 surname: 'Akhunov',
                 patrName: 'Akhmatovich'
             },
-            surname:'Akhunov',
-            name:'Azat',
-            patrName:'Akhmatovich',
-            nickname:'leon4uk',
-            dbirth:'12.07.1986',
+            surname: 'Akhunov',
+            name: 'Azat',
+            patrName: 'Akhmatovich',
+            nickname: 'leon4uk',
+            dbirth: '12.07.1986',
             sex: "Мужской",
-            language: "Русский"
+            language: "Русский",
+            city: "Москва, Россия"
         };
         this.loadUser = this.loadUser.bind(this);
         this.reload = this.reload.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this._isMounted = true;
-        const { handle } = this.props.match.params;
+        const {handle} = this.props.match.params;
         //this.setState({user: this.props.currentUser})
         // this.loadUser(handle);
     }
 
 
-    loadUser(handle){
+    loadUser(handle) {
         this.setState({
             loading: true
         });
@@ -64,7 +64,7 @@ class Profile extends Component {
         this._isMounted = false;
     }
 
-    reload (){
+    reload() {
         window.location.reload();
     };
 
@@ -78,7 +78,7 @@ class Profile extends Component {
         });
     }
 
-    handleDropdownChange = (e, { key, value }) => this.setState({ [key]: value });
+    handleDropdownChange = (e, {key, value}) => this.setState({[key]: value});
 
     render() {
         const sexOptions = [
@@ -105,15 +105,57 @@ class Profile extends Component {
                 value: 'English'
             }
         ];
+        const cityOptions = [
+            {
+                city: 'Москва, Россия',
+                text: 'Москва, Россия',
+                value: 'Москва, Россия'
+            },
+            {
+                city: 'Казань, Россия',
+                text: 'Казань, Россия',
+                value: 'Казань, Россия'
+            },
+            {
+                city: 'Набережные Челны, Россия',
+                text: 'Набережные Челны, Россия',
+                value: 'Набережные Челны, Россия'
+            },
+            {
+                city: 'Мытищи, Россия',
+                text: 'Мытищи, Россия',
+                value: 'Мытищи, Россия'
+            },
+            {
+                city: 'Анапа, Россия',
+                text: 'Анапа, Россия',
+                value: 'Анапа, Россия'
+            },
+            {
+                city: 'Лабытнанги, Россия',
+                text: 'Лабытнанги, Россия',
+                value: 'Лабытнанги, Россия'
+            },
+            {
+                city: 'Braunschweig, Germany',
+                text: 'Braunschweig, Germany',
+                value: 'Braunschweig, Germany'
+            },
+            {
+                city: 'Reykjavík, Iceland',
+                text: 'Reykjavík, Iceland',
+                value: 'Reykjavík, Iceland'
+            }
+        ];
         return (
             <div className="profile-main">
                 <div className="profile-main-container">
                     <div className="container-breadcrumb">
                         <Breadcrumb>
                             <Breadcrumb.Section as={NavLink} to={'/'} link>Главная</Breadcrumb.Section>
-                            <Breadcrumb.Divider icon='right chevron' />
+                            <Breadcrumb.Divider icon='right chevron'/>
                             <Breadcrumb.Section as={NavLink} to={'/profile'} link>Личный кабинет</Breadcrumb.Section>
-                            <Breadcrumb.Divider icon='right arrow' />
+                            <Breadcrumb.Divider icon='right arrow'/>
                             <Breadcrumb.Section active>Настройки профиля</Breadcrumb.Section>
                         </Breadcrumb>
                     </div>
@@ -122,7 +164,8 @@ class Profile extends Component {
                             <div className="profile-avatar">
                                 {
                                     this.state.user.imageUrl ? (
-                                        <Image src={this.state.user.imageUrl} size='medium' circular verticalAlign='top' alt={this.state.user.name}/>
+                                        <Image src={this.state.user.imageUrl} size='medium' circular verticalAlign='top'
+                                               alt={this.state.user.name}/>
                                     ) : (
                                         <div className="text-avatar">
                                             <span>{this.state.user.name && this.state.user.name[0]}</span>
@@ -130,7 +173,7 @@ class Profile extends Component {
                                     )
                                 }
                                 <div className="profile-avatar-footer">
-                                    <Icon link name='photo' size={'large'} color={'grey'} />
+                                    <Icon link name='photo' size={'large'} color={'grey'}/>
                                 </div>
                             </div>
                             <div className="user-name-container">
@@ -139,7 +182,7 @@ class Profile extends Component {
                                 <span style={{paddingRight: '8px'}}>{this.state.user.patrName}</span>
                             </div>
                             <div className="user-custom-icon-container">
-                                <Icon link name='cog' size={'large'} color={'grey'} />
+                                <Icon link name='cog' size={'large'} color={'grey'}/>
                             </div>
                         </div>
                         <div className="profile-info-container">
@@ -149,44 +192,62 @@ class Profile extends Component {
                             <div className="profile-info-container-name-inputs">
                                 <div className="profile-info-container-name-input">
                                     <label>Фамилия</label>
-                                    <Input onChange={this.handleInputChange} defaultValue={this.state.surname} className="form-input" id="surname"
+                                    <Input onChange={this.handleInputChange} defaultValue={this.state.surname}
+                                           className="form-input" id="surname"
                                            name="surname" required placeholder='Фамилия'/>
                                 </div>
                                 <div className="profile-info-container-name-input">
                                     <label>Имя</label>
-                                    <Input onChange={this.handleInputChange} defaultValue={this.state.name} className="form-input" id="name"
+                                    <Input onChange={this.handleInputChange} defaultValue={this.state.name}
+                                           className="form-input" id="name"
                                            name="name" required placeholder='Имя'/>
                                 </div>
                                 <div className="profile-info-container-name-input">
                                     <label>Отчество</label>
-                                    <Input onChange={this.handleInputChange} defaultValue={this.state.patrName} className="form-input" id="patrName"
+                                    <Input onChange={this.handleInputChange} defaultValue={this.state.patrName}
+                                           className="form-input" id="patrName"
                                            name="patrName" required placeholder='Отчество'/>
                                 </div>
                             </div>
                             <div className="profile-info-container-nickname-input">
                                 <div className="profile-info-container-name-input">
                                     <label>Имя профиля</label>
-                                    <Input onChange={this.handleInputChange} defaultValue={this.state.nickname} className="form-input" id="nickname"
+                                    <Input onChange={this.handleInputChange} defaultValue={this.state.nickname}
+                                           className="form-input" id="nickname"
                                            name="nickname" required placeholder='Имя профиля'/>
                                 </div>
                             </div>
                             <div className="profile-info-container-date-birth-input">
                                 <div className="profile-info-container-name-input">
                                     <label>Дата рождения</label>
-                                    <Input onChange={this.handleInputChange} defaultValue={this.state.dbirth} className="form-input" id="dbirth"
+                                    <Input onChange={this.handleInputChange} defaultValue={this.state.dbirth}
+                                           className="form-input" id="dbirth"
                                            name="dbirth" required placeholder='Дата рождения'/>
                                 </div>
                             </div>
                             <div className="profile-info-container-sex-input">
                                 <div className="profile-info-container-name-input">
                                     <label style={{paddingBottom: '6px'}}>Пол</label>
-                                    <Dropdown onChange={this.handleDropdownChange} placeholder='Пол' fluid selection id="sex" name="sex" className="form-input"  options={sexOptions} defaultValue={this.state.sex}/>
+                                    <Dropdown onChange={this.handleDropdownChange} placeholder='Пол' fluid selection
+                                              id="sex" name="sex" className="form-input" options={sexOptions}
+                                              defaultValue={this.state.sex}/>
                                 </div>
                             </div>
                             <div className="profile-info-container-input">
                                 <div className="profile-info-container-name-input">
                                     <label style={{paddingBottom: '6px'}}>Язык</label>
-                                    <Dropdown onChange={this.handleDropdownChange} placeholder='Язык' fluid selection id="language" name="language" className="form-input" options={languageOptions} defaultValue={this.state.language}/>
+                                    <Dropdown onChange={this.handleDropdownChange} placeholder='Язык' fluid selection
+                                              id="language" name="language" className="form-input"
+                                              options={languageOptions} defaultValue={this.state.language}/>
+                                </div>
+                            </div>
+                            <div className="profile-info-container-input">
+                                <div className="profile-info-container-name-input">
+                                    <label style={{paddingBottom: '6px'}}>Город</label>
+                                    <Dropdown onChange={this.handleDropdownChange} placeholder='Город' fluid search
+                                              selection id="city" name="city" noResultsMessage="Москва - лучший город"
+                                              className="form-input" options={cityOptions}
+                                              defaultValue={this.state.city}/>
                                 </div>
                             </div>
                         </div>
