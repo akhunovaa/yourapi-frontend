0534c355f4a67b8e98db335056eb4d9e6ae4e2d9
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-09 17:31:04 +0300

    [CF] color and other styles updated

--- src/login/Login.js
+++ src/login/Login.js
@@ -44,7 +44,7 @@ class Login extends Component {
                 </div>
                 <div id="login-container-right">
                     <div id="login-container-right-header">
-                        <Header as='h3' color='blue' className={'login-right-header'}>YourAPI</Header>
+                        <Header as='h3' className={'login-right-header'}>YourAPI</Header>
                     </div>
                     <div id="login-container-right-form">
                         <div className='navigate-links'>
@@ -146,7 +146,7 @@ class LoginForm2 extends Component {
                                         paddingBottom: '16px'
                                     }} label='Запомнить меня'/>
                                 </Form.Field>
-                                <Button type='submit' style={{background: '#2F80ED', color: 'white'}} fluid
+                                <Button type='submit' className='submit-button' fluid
                                         size='large' onClick={this.handleSubmit}>
                                     Войти
                                 </Button>
