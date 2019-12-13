4a1bffa019b722c0e0eaf2214654a365490c65bb
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-13 18:50:27 +0300

    [NF] size of login form fixed

--- src/signup/SignUp.js
+++ src/signup/SignUp.js
@@ -114,7 +114,7 @@ class SignupForm2 extends Component {
 
     render() {
         return (
-            <Grid style={{paddingLeft: '80px', paddingRight: '80px'}} textAlign='center'>
+            <Grid textAlign='center'>
                 <Grid.Column width={16} widescreen={20} tablet={16} mobile={16} largeScreen={25} computer={20} stretched>
                     <Form size='tiny' onSubmit={this.handleSubmit}>
                         <Segment className='login-data-segment-form'>
