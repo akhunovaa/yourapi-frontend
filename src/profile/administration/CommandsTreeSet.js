8cf0d0d1edef18d23c72ead67e94e1f0ce238e0e
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-17 18:30:13 +0300

    [CF] list hide fixed

--- src/profile/administration/CommandsTreeSet.js
+++ src/profile/administration/CommandsTreeSet.js
@@ -55,7 +55,7 @@ class CommandsTreeSet extends Component {
                             </List.Content>
                             <Icon name='users' className={naming !== 'Волга' ? 'command-disabled-color' : null} />
                             <List.Content><span className={classNames({ 'command-disabled-color' : naming !== 'Волга'}, 'command-operation-text', 'command-bold-font-weight')}>Волга</span></List.Content>
-                            <List.List>
+                            <List.List hidden={this.state.hidden}>
                                 <List.Content className='sub-command'>
                                     <NavLink to="/profile/administration?company=Волга&page=about"><span className={classNames({ 'command-disabled-color' : naming !== 'Волга'}, { 'command-link-color' : (naming === 'Волга' && page === 'about')}, 'command-operation-text')}>О Команде</span></NavLink>
                                 </List.Content>
@@ -71,7 +71,7 @@ class CommandsTreeSet extends Component {
                             </List.Content>
                             <Icon name='users' className={naming !== 'Урал' ? 'command-disabled-color' : null} />
                             <List.Content><span className={classNames({ 'command-disabled-color' : naming !== 'Урал'}, 'command-operation-text', 'command-bold-font-weight')}>Урал</span></List.Content>
-                            <List.List>
+                            <List.List hidden={this.state.hidden2}>
                                 <List.Content className='sub-command'>
                                     <NavLink to="/profile/administration?company=Урал&page=about"><span className={classNames({ 'command-disabled-color' : naming !== 'Урал'}, { 'command-link-color' : (naming === 'Урал' && page === 'about')}, 'command-operation-text')}>О Команде</span></NavLink>
                                 </List.Content>
