fa6d0f0f4fed132e8d7dd7cc09181558bc5f33c1
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-18 01:18:21 +0300

    [NF] footer data added

--- src/common/PrivateRoute.js
+++ src/common/PrivateRoute.js
@@ -2,12 +2,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Route, Redirect, withRouter} from 'react-router-dom';
 
-import {
-    Route,
-    Redirect
-} from "react-router-dom";
-
-
 const PrivateRoute = ({ component: Component, authenticated, ...rest }) => (
     <Route
         {...rest}
@@ -30,5 +24,4 @@ const mapStateToProps = state => ({
         authenticated : state.authenticated
     });
 
-export default withRouter(connect(
-    mapStateToProps, null, null, {pure: false})(PrivateRoute));
+export default withRouter(connect(mapStateToProps, null, null, {pure: false})(PrivateRoute));
