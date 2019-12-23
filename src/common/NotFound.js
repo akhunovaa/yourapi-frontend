2280536e2c18530df65972de46a75885f2b7df81
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-23 21:45:33 +0300

    [NF] 404 data added

--- src/common/NotFound.js
+++ src/common/NotFound.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import './NotFound.css';
-import { Link } from 'react-router-dom';
+import {Button, Icon} from "semantic-ui-react";
+import {NavLink} from "react-router-dom";
 
 class NotFound extends Component {
 
     render() {
         return (
                     <div className="page-not-found">
-                        <h1 className="title">
+                        <h1 className="page-not-found-title-2">
                             404
                         </h1>
-                        <div className="desc">
-                            Страница, которую вы искали не найдена.
+                        <h1 className="page-not-found-title">
+                            404
+                        </h1>
+                        <div className="page-not-found-desc">
+                         <p>Страница
+                        </p>не найдена
                         </div>
-                        <Link to="/"><button className="go-back-btn btn btn-primary" type="button">Вернуться...</button></Link>
+                        <div className='page-not-found-go-back-btn'>
+                            <NavLink to="/"><Button style={{background: '#F39847'}}><span className='go-back-404-text-button'>Вернуться назад</span></Button></NavLink>
+                        </div>
                     </div>
         );
     }
