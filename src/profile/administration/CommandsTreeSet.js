3cbb292881e9748792be934187c9e8a549e4dc2b
Azat Akhunov <azat.akhunov@protonmail.com>
2019-12-23 19:16:43 +0300

    [NF] company data added

--- src/profile/administration/CommandsTreeSet.js
+++ src/profile/administration/CommandsTreeSet.js
@@ -76,7 +76,7 @@ class CommandsTreeSet extends Component {
                                     <NavLink to="/profile/administration?company=Урал&page=about"><span className={classNames({ 'command-disabled-color' : naming !== 'Урал'}, { 'command-link-color' : (naming === 'Урал' && page === 'about')}, 'command-operation-text')}>О компании</span></NavLink>
                                 </List.Content>
                                 <List.Content className='sub-command'>
-                                    <NavLink to="/profile/administration?company=Урал&page=members"><span className={classNames({ 'command-disabled-color' : naming !== 'Урал'}, { 'command-link-color' : (naming === 'Урал' && page === 'members')}, 'command-operation-text')}>Участники</span></NavLink>
+                                    <NavLink to="/profile/administration?company=Урал&page=members"><span className={classNames({ 'command-disabled-color' : naming !== 'Урал'}, { 'command-link-color' : (naming === 'Урал' && page === 'members')}, 'command-operation-text')}>Сотрудники</span></NavLink>
                                 </List.Content>
                             </List.List>
                         </List.Item>
