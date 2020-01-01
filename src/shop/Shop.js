d53f22d2b1a2ba88351a7e9e3bb953b4ddda496e
Azat Akhunov <azat.akhunov@protonmail.com>
2020-01-01 16:57:30 +0300

    [NF] footer data added

--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import './Shop.css';
 import {NavLink} from "react-router-dom";
-import {Breadcrumb} from "semantic-ui-react";
+import {Breadcrumb, Icon} from "semantic-ui-react";
 import {loadUser} from "../util/APIUtils";
+import Slider from '@material-ui/core/Slider';
+import {Table} from "semantic-ui-react/dist/commonjs/collections/Table";
+
 
 class Shop extends Component {
 
@@ -10,11 +13,15 @@ class Shop extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            responseScale: [0, 1000],
+            responseStableScale: [0, 100]
+        };
         this.loadUser = this.loadUser.bind(this);
         this.reload = this.reload.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleOnPhoneChange = this.handleOnPhoneChange.bind(this);
+        this.valuetext = this.valuetext.bind(this);
     }
 
     componentDidMount() {
@@ -76,6 +83,23 @@ class Shop extends Component {
         this.setState({open: true})
     };
 
+    valuetext(value) {
+        return `${value}mS`;
+    }
+
+
+    handleSliderChange = (event, newValue) => {
+        this.setState({
+            responseScale: newValue
+        });
+    };
+
+    handleStableSliderChange = (event, newValue) => {
+        this.setState({
+            responseStableScale: newValue
+        });
+    };
+
     handleClose = () => {
         this.setState({open: false})
     };
@@ -84,15 +108,115 @@ class Shop extends Component {
 
         return (
             <div className="shop-main">
+                <div className="shop-container-breadcrumb">
+                    <Breadcrumb>
+                        <Breadcrumb.Section as={NavLink} to={'/'} link><span
+                            className='text-disabled-color'>Главная</span></Breadcrumb.Section>
+                        <Breadcrumb.Divider icon='right chevron'/>
+                        <Breadcrumb.Section as={NavLink} to={'/shop'} link><span
+                            className='text-disabled-color'>Магазин</span></Breadcrumb.Section>
+                    </Breadcrumb>
+                </div>
                 <div className="shop-main-container">
-                    <div className="shop-container-breadcrumb">
-                        <Breadcrumb>
-                            <Breadcrumb.Section as={NavLink} to={'/'} link><span
-                                className='text-disabled-color'>Главная</span></Breadcrumb.Section>
-                            <Breadcrumb.Divider icon='right chevron'/>
-                            <Breadcrumb.Section as={NavLink} to={'/shop'} link><span
-                                className='text-disabled-color'>Магазин</span></Breadcrumb.Section>
-                        </Breadcrumb>
+
+                    <div className="shop-left-container">
+                        <div className='shop-inner-filter-container'>
+                            <div className='shop-filter-title'>Фильтры</div>
+                            <div className='shop-filter-rating'>Рейтинг</div>
+                            <div className='shop-filter-rating-stars'>
+                                    <Icon link name='star outline' className='star'/>
+                                    <Icon link name='star outline' className='star'/>
+                                    <Icon link name='star outline' className='star'/>
+                                    <Icon link name='star outline' className='star'/>
+                                    <Icon link name='star outline' className='star'/>
+                            </div>
+                            <div className='shop-filter-response-time'>Скорость отдачи</div>
+                            <div className='shop-filter-response-time-label'>
+                                <span className='left-label'>{this.state.responseScale[0] + 'mS'}</span>
+                                <span className='right-label'>{this.state.responseScale[1] + 'mS'}</span>
+                            </div>
+                            <div className='shop-filter-response-time-line'>
+                                <Slider
+                                    className='shop-filter-response-time-line-slider'
+                                    value={this.state.responseScale}
+                                    onChange={this.handleSliderChange}
+                                    valueLabelDisplay="off"
+                                    aria-labelledby="range-slider"
+                                    min={0}
+                                    max={1000}
+                                />
+                            </div>
+                            <div className='shop-filter-response-time'>Стабильность</div>
+                            <div className='shop-filter-response-time-label'>
+                                <span className='left-label'>{this.state.responseStableScale[0] + '%'}</span>
+                                <span className='right-label'>{this.state.responseStableScale[1] + '%'}</span>
+                            </div>
+                            <div className='shop-filter-response-time-line'>
+                                <Slider
+                                    className='shop-filter-response-time-line-slider'
+                                    value={this.state.responseStableScale}
+                                    onChange={this.handleStableSliderChange}
+                                    valueLabelDisplay="off"
+                                    aria-labelledby="range-slider"
+                                    min={0}
+                                    max={100}
+                                />
+                            </div>
+                            <div className='shop-filter-response-time'>Категории</div>
+                            <div className='category-labels'>
+                                <div className='category-label'>
+                                    <Icon color='red' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Данные</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='orange' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Финансы</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='yellow' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Мобильные</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='green' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Карты</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='olive' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Реклама</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='blue' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Социальные сети</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='purple' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Здравохранение</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='teal' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Спорт</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='violet' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Web</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                                <div className='category-label'>
+                                    <Icon color='grey' name='dot circle' size='small'/>
+                                    <NavLink to='#'>Другое</NavLink>
+                                    <span className='right-label'>45</span>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <div className="shop-form-container">
 
