9110a74320b406079e0f9bf72ceea285ee84593d
Azat Akhunov <azat.akhunov@protonmail.com>
2020-01-03 13:49:29 +0300

    [NF] header data added

--- src/common/AppHeader.js
+++ src/common/AppHeader.js
@@ -30,6 +30,26 @@ class AppHeader extends Component {
                 return <AdministrationHeader onLogout={this.props.onLogout}/>;
             case '/profile/api':
                 return <ApiHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/data':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/finance':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/mobile':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/map':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/adv':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/social':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/health':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/sport':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/web':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
+            case '/shop/category/other':
+                return <ShopCategoryHeader onLogout={this.props.onLogout}/>;
             default:
                 return <HomeHeader onLogout={this.props.onLogout}/>;
         }
@@ -198,7 +218,7 @@ class IntegratorHeader extends Component {
                         <Link to="/shop" style={{color: '#A5A5A5'}}>Магазин</Link>
                     </div>
                     <div className='header-center-navlink-integration active-header'>
-                        <Link to="/integrator">Интеграторы</Link>
+                        <Link to="/integrator" style={{color: '#4F4F4F'}}>Интеграторы</Link>
                     </div>
                     <div className='header-center-search-input'>
                         <Input size={'small'} fluid icon={{ name: 'search', link: true}}
@@ -315,7 +335,7 @@ class ShopHeader extends Component {
                 </div>
                 <div className='header-center-container'>
                     <div className='header-center-navlink-merch active-header'>
-                        <Link to="/shop">Магазин</Link>
+                        <Link to="/shop" style={{color: '#4F4F4F'}}>Магазин</Link>
                     </div>
                     <div className='header-center-navlink-integration inactive-header-link'>
                         <Link to="/integrator" style={{color: '#A5A5A5'}}>Интеграторы</Link>
@@ -405,4 +425,123 @@ class ShopHeader extends Component {
     }
 }
 
+class ShopCategoryHeader extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        };
+    }
+
+    handleOpen = () => {
+        this.setState({open: true})
+    };
+
+    handleClose = () => {
+        this.setState({open: false})
+    };
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        const { open } = this.state;
+        return (
+            <div className="header-authenticated">
+                <div className='header-left-logo active-logo'>
+                    <Link to="/">YourAPI</Link>
+                </div>
+                <div className='header-center-container'>
+                    <div className='header-center-navlink-merch active-header no-border-header-link'>
+                        <Link to="/shop" style={{color: '#4F4F4F'}}>Магазин</Link>
+                    </div>
+                    <div className='header-center-navlink-integration inactive-header-link'>
+                        <Link to="/integrator" style={{color: '#A5A5A5'}}>Интеграторы</Link>
+                    </div>
+                    <div className='header-center-search-input'>
+                        <Input size={'small'} fluid icon={{ name: 'search', link: true}}
+                               className='header-center-search' placeholder='Поиск...' id="search" name="search"/>
+                    </div>
+                </div>
+                <div className="header-right-menu">
+                    <div className='header-right-center-container'>
+                        <div className='api-create-button-container'>
+                            <NavLink to='/profile/api?page=add' ><Button fluid className='api-create-button' style={{background: '#F39847'}}><span className='api-create-button-text'>Добавить API</span></Button></NavLink>
+                        </div>
+                    </div>
+                    <div className='header-right-navlink'>
+                        <Dropdown text='RU' closeOnChange>
+                            <Dropdown.Menu>
+                                <Dropdown.Item text='RU' description='Русский' />
+                                <Dropdown.Item text='EN' description='English' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className='header-right-navlink-bell'>
+                        <Icon link size={'large'}  name='bell outline' />
+                    </div>
+                    <div className='header-right-navlink-bookmark'>
+                        <Icon link size={'large'}  name='bookmark outline' />
+                    </div>
+                    <div className='header-right-navlink-profile'>
+                        <Portal
+                            closeOnPortalMouseLeave
+                            closeOnTriggerClick
+                            closeOnDocumentClick
+                            trigger={
+                                <Icon link name='user circle' />
+                            }
+                            open={open}
+                            onOpen={this.handleOpen}
+                            onClose={this.handleClose}>
+                            <div id='profile-portal' onClick={this.handleClose}>
+                                <Segment className="profile-segment"
+                                         style={{position: 'fixed',  right: 12,  top: '76px', zIndex: 1000}}>
+                                    <List size={"big"}>
+                                        <List.Item>
+                                            <List.Content>
+                                                <NavLink to="/"><span className="portal-item portal-item-main">Личный кабинет</span></NavLink>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <NavLink to="/profile"><span className="portal-item">Настройка профиля</span></NavLink>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <NavLink to="/profile/administration"><span className="portal-item">Администрирование</span></NavLink>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <Divider style={{marginTop: 0,  marginBottom: 0, paddingTop: 0, paddingBottom: 0}}/>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <NavLink to="/"><span className="portal-item">Справка</span></NavLink>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <Divider style={{marginTop: 0,  marginBottom: 0, paddingTop: 0, paddingBottom: 0}}/>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <a onClick={this.props.onLogout}>
+                                                    <span className="portal-item">Выйти</span>
+                                                </a>
+                                            </List.Content>
+                                        </List.Item>
+                                    </List>
+                                </Segment>
+                            </div>
+                        </Portal>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
 export default withRouter(AppHeader);
