ab127d0d8af1bf1a1ff3835310ca987892fe82d2
Azat Akhunov <azat.akhunov@protonmail.com>
2020-02-24 23:43:29 +0300

    [NF] footer data added

--- src/util/APIUtils.js
+++ src/util/APIUtils.js
@@ -138,8 +138,8 @@ export function feedback(feedbackRequest) {
 
 export function apiProjectListGet() {
     return requestGet({
-        //url: API_BASE_URL + "/api-data/list",
-        url: "http://localhost:7100" + "/api-data/list",
+        url: API_BASE_URL + "/api-data/list",
+        //url: "http://localhost:7100" + "/api-data/list",
         method: 'GET'
     });
 }
