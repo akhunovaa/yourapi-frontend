d073867222bf85ca5ff0f9105c1928a42ff9bc49
Azat Akhunov <azat.akhunov@protonmail.com>
2020-02-25 15:26:27 +0300

    [NF] footer data added

--- src/profile/api/ApiTreeSet.js
+++ src/profile/api/ApiTreeSet.js
@@ -14,7 +14,7 @@ class ApiTreeSet extends Component {
         this.state = {
             hidden: true,
             arrow: 'chevron down',
-            projects:[]
+            projects: [{"id":22222,"name":"fd","category":"Спорт","banned":false,"approved":false,"private":true,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":9,"name":"yourapi.ru","description":"ref={this.image} onLoad={this.handleImageLoaded}","category":"Спорт","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":1,"name":"Second Test API","description":"This is a second API for a BIG start!","category":"Спорт","banned":true,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":7,"name":"API of DEV","description":"Краткое первое описание из DEV стенда 24 февраля 2020 года в 13:34\n\nКиша рядом и Warcraft 3 reforged )","category":"Спорт","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":4,"name":"Api with smart pictures","description":"Разработать сервис по сохранению openapi файла версии 3.0\nyaml и json. Примеры файлов прилагаются.","category":"Новости","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":5,"name":"Weather","description":"Разработать сервис по сохранению openapi файла версии 3.0\nyaml и json. Примеры файлов прилагаются.","category":"Новости","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":3,"name":"First Test API","description":"This is a 111 API for a BIG start!","category":"Новости","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":2,"name":"First Test API","description":"This is a 111 API for a BIG start!","category":"Новости","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":8,"name":"Первый API у Леночки","description":"Рядом спит Кишука","category":"Новости","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":6,"name":"API of Weather","description":"Краткое первое описание из DEV стенда 24 февраля 2020 года в 13:34\nКиша рядом и Warcraft 3 reforged )","category":"Спорт","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}},{"id":10,"name":"33.yourapi.ru","description":"","category":"Социальные сети","banned":false,"approved":false,"private":false,"username":{"id":2,"username":"admin","email":"azat.akhunov@protonmail.com"}}]
         };
         this.toggle = this.toggle.bind(this);
         this.handleCheck = this.handleCheck.bind(this);
@@ -22,7 +22,7 @@ class ApiTreeSet extends Component {
 
     componentDidMount() {
         this._isMounted = true;
-        if (!this.state.projects) return;
+        if (this.state.projects) return;
         apiProjectListGet()
             .then(response => {
                 if (this._isMounted) {
@@ -33,7 +33,6 @@ class ApiTreeSet extends Component {
             }).catch(error => {
             Alert.error('Ошибка получения списка проектов' || (error && error.message));
         });
-        console.log(this.state.projects)
     }
 
     toggle(event) {
@@ -62,22 +61,18 @@ class ApiTreeSet extends Component {
         }
         const pagingArray = ['about', 'members', 'list', 'update'];
         const params = queryString.parse(this.props.location.search);
-        let naming = (params.company !== 'undefined' && this.handleCheck(namingArray, params.company)) ? params.company : 'API-FOOTBALL';
-
-
-
+        let naming = (params.name !== 'undefined' && this.handleCheck(namingArray, params.name)) ? params.name : 'API-FOOTBALL';
         const Projects = ({items}) => (
             <>
                 {
                     items.map(item => (
-                            <List.Item key={item.id} style={{paddingTop: 28, paddingLeft: 22}}>
+                            <List.Item key={item.id} style={{paddingTop: 28, paddingLeft: 22, whiteSpace: 'nowrap'}}>
                                 <List.Content floated='right'>
-                                    <Icon link id='currency' name={this.state.arrow} onClick={this.toggle}
-                                          className={naming !== item.name ? 'api-disabled-color' : null}/>
+                                    <Icon link id='currency' name={this.state.arrow} onClick={this.toggle} className={naming !== item.name ? 'api-disabled-color' : null}/>
                                 </List.Content>
-                                <List.Content><span
-                                    className={classNames({'api-disabled-color': naming !== item.name}, 'api-command-operation-text')}>{item.name}</span></List.Content>
-                                <List.List hidden='false'>
+                                <List.Content>
+                                    <span className={classNames({'api-disabled-color': naming !== item.name}, 'api-command-operation-text')}>{item.name}</span></List.Content>
+                                <List.List hidden={false}>
                                     <List.Content className='sub-command'>
                                         <NavLink to={"/profile/api?name=" + item.name + "&page=update"}><span
                                             className={classNames({'api-disabled-color': naming !== item.name}, {'api-enabled-color': (naming === item.name && page === 'update')}, 'api-command-operation-text')}>Редактировать</span></NavLink>
