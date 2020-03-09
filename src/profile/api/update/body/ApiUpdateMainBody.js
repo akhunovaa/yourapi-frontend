43e6edc732728de318773bb79540142904bb4fea
Azat Akhunov <azat.akhunov@protonmail.com>
2020-03-09 21:00:24 +0300

    [NF] api update body data added

---
+++ src/profile/api/update/body/ApiUpdateMainBody.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+
+class ApiUpdateMainBody extends Component {
+
+    _isMounted = false;
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.reload = this.reload.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleCheck = this.handleCheck.bind(this);
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    reload() {
+        window.location.reload();
+    };
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
+
+        this.setState({
+            [inputName]: inputValue
+        });
+    }
+
+    handleDropdownChange = (e, {key, value}) => this.setState({[key]: value});
+
+    handleCheck(array, val) {
+        return array.some(item => item === val);
+    }
+
+    handleClose = () => {
+        this.setState({open: false})
+    };
+
+
+    render() {
+        return (
+            <div>
+              <h1></h1>
+              <h1></h1>
+              <h1></h1>
+              <h1>Основные</h1>
+            </div>
+
+        )
+    }
+}
+
+export default ApiUpdateMainBody;
