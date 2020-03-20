2535ed4f63c8e8a086d7299bedeafa74ff77fe41
Azat Akhunov <azat.akhunov@protonmail.com>
2020-03-21 01:24:46 +0300

    [NF] great image uploader added

---
+++ src/dropzone/ApiImageDropzone.js
@@ -0,0 +1,127 @@
+import React, {Component} from "react";
+import "./Dropzone.css";
+import error from "../img/error.png";
+import {Image} from "semantic-ui-react";
+
+class ApiImageDropzone extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {hightlight: false};
+        this.imageInputRef = React.createRef();
+
+        this.openFileDialog = this.openFileDialog.bind(this);
+        this.onFilesAdded = this.onFilesAdded.bind(this);
+        this.onDragOver = this.onDragOver.bind(this);
+        this.onDragLeave = this.onDragLeave.bind(this);
+        this.onDrop = this.onDrop.bind(this);
+        this.renderErrorProgress = this.renderErrorProgress.bind(this);
+        this.renderErrorState = this.renderErrorState.bind(this);
+        this.onFileAdded = this.onFileAdded.bind(this);
+        this.hasExtension = this.hasExtension.bind(this);
+    }
+
+    openFileDialog() {
+        if (this.props.disabled) return;
+        this.imageInputRef.current.click();
+    }
+
+    onFilesAdded(evt) {
+        if (this.props.disabled) return;
+        const file = evt.target.files[0];
+        this.setState({imageFile: file});
+        this.onFileAdded(file)
+    }
+
+    onDragOver(event) {
+        event.preventDefault();
+        if (this.props.disabed) return;
+        this.setState({hightlight: true});
+    }
+
+    onDragLeave(event) {
+        this.setState({hightlight: false});
+    }
+
+    onDrop(event) {
+        event.preventDefault();
+        if (this.props.disabed) return;
+        const file = event.dataTransfer.files[0];
+        this.onFileAdded(file)
+        this.setState({hightlight: false});
+    }
+
+    onFileAdded(file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            this.setState({
+                imageUrl: reader.result
+            })
+        };
+        reader.readAsDataURL(file);
+        this.setState({
+            imageUrl: reader.result
+        });
+    }
+
+
+    renderErrorProgress() {
+        return (
+            <div className="ProgressWrapper">
+                <div className="error-progressBar"/>
+                <img
+                    className="CheckIcon"
+                    alt="File format error"
+                    src={error}
+                />
+            </div>
+        );
+    }
+
+    renderErrorState() {
+        if (!this.props.hasErrorFiles) {
+            this.props.setErrorFileState(true)
+        }
+    }
+
+    hasExtension(fileName, exts) {
+        return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(fileName);
+    }
+
+    render() {
+        return (
+            <div className="image-upload">
+                <div className='api-image-upload-container'>
+                    <div
+                        className={`api-image-Dropzone ${this.state.hightlight ? "Highlight" : ""}`}
+                        onDragOver={this.onDragOver}
+                        onDragLeave={this.onDragLeave}
+                        onDrop={this.onDrop}
+                        onClick={this.openFileDialog}
+                        style={{cursor: this.props.disabled ? "default" : "pointer"}}>
+                        <div className="api-project-avatar">
+                            {
+                                !this.hasExtension(this.state.imageFile, ['.jpg', '.jpeg', '.png']) && this.state.imageUrl ? (
+                                    <Image src={this.state.imageUrl} size='medium' circular verticalAlign='top'
+                                           alt={this.props.apiName}/>
+                                ) : (
+                                    <div className="api-text-avatar">
+                                        <span>{this.props.apiName && this.props.apiName[0]}</span>
+                                    </div>
+                                )
+                            }
+                        </div>
+                        <input
+                            accept="image/x-png,image/jpeg"
+                            ref={this.imageInputRef}
+                            className="FileInput"
+                            type="file"
+                            onChange={this.onFilesAdded}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ApiImageDropzone;
