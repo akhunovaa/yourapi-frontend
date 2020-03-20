469ee189785d63a80fa701cee052f3eceeed7025
Azat Akhunov <azat.akhunov@protonmail.com>
2020-03-20 13:18:47 +0300

    [NF] footer data added

--- src/home/Home.js
+++ src/home/Home.js
@@ -26,6 +26,7 @@ class Home extends Component {
 
     componentDidMount(){
         console.log(process.env.REACT_APP_API_URL)
+        console.log(process.env.NODE_ENV)
         this._isMounted = true;
         this.setState({
             loading: true
