616960439cbf51861862ad0d12a43ded8ad2c206
Azat Akhunov <azat.akhunov@protonmail.com>
2020-03-21 16:17:52 +0300

    [NF] elements purged

--- src/common/AppFooter.js
+++ src/common/AppFooter.js
@@ -11,7 +11,7 @@ class AppFooter extends Component {
                 {this.props.authenticated ? (
                     <div className="footer-authenticated">
                         <div className='footer-left-logo'>
-                            <NavLink to="/"><span style={{color: '#F2F2F2'}}>© 2019 YourAPI</span></NavLink>
+                            <NavLink to="/"><span style={{color: '#F2F2F2'}}>© 2019-2020 YourAPI</span></NavLink>
                         </div>
 
                         <div className="footer-link-container">
