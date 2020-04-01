b437c53179d11a00e5df9cead0d80f5a5c6a838a
Nikolai ADIUTANTOV <nikolai.adiutantov@yandex.ru>
2020-04-01 23:13:55 +0300

    just simple refactoring i'll plan to use PropTypes everywhere, moreover it'll be better to use special variable for style that contains all properties for all elements

--- src/common/AppFooter.js
+++ src/common/AppFooter.js
@@ -3,57 +3,75 @@ import './AppFooter.css';
 import {NavLink} from "react-router-dom";
 import {Icon, Dropdown} from "semantic-ui-react";
 import {CircleArrow as ScrollUpButton} from "react-scroll-up-button";
+import PropTypes from 'prop-types';
 
 class AppFooter extends Component {
     render() {
+        const {authenticated} = this.props;
+        var social = ["twitter", "linkedin", "facebook", "vk"];
+
+        var styles = {
+            spanOfContainer: {
+                color: '#F2F2F2'
+            },
+            ScrollUpButton: {
+                bottom: 50,
+                backgroundColor: 'none',
+                border: 'none'
+            }
+        };
+
         return (
             <div>
-                {this.props.authenticated ? (
+                {authenticated ? (
                     <div className="footer-authenticated">
                         <div className='footer-left-logo'>
-                            <NavLink to="/"><span style={{color: '#F2F2F2'}}>© 2019-2020 YourAPI</span></NavLink>
+                            <NavLink to="/"><span style={styles.spanOfContainer}>© 2019-2020 YourAPI</span></NavLink>
                         </div>
 
                         <div className="footer-link-container">
-                            <a href="/privacy.html" target="_blank" rel="noopener noreferrer"><span style={{color: '#F2F2F2'}}>Политика конфиденциальности</span></a>
+                            <a href="/privacy.html" target="_blank" rel="noopener noreferrer">
+                                <span style={styles.spanOfContainer}>Политика конфиденциальности</span>
+                            </a>
                         </div>
                         <div className="footer-link-container">
-                            <a href="/agreement.html" target="_blank" rel="noopener noreferrer"><span style={{color: '#F2F2F2'}}>Пользовательское соглашение</span></a>
+                            <a href="/agreement.html" target="_blank" rel="noopener noreferrer">
+                                <span style={styles.spanOfContainer}>Пользовательское соглашение</span>
+                            </a>
                         </div>
                         <div className="footer-link-container">
-                            <a href={"mailto:support@yourapi.ru"} rel="noopener noreferrer"><span style={{color: '#F2F2F2'}}>Служба поддержки</span></a>
+                            <a href={"mailto:support@yourapi.ru"} rel="noopener noreferrer">
+                                <span style={styles.spanOfContainer}>Служба поддержки</span>
+                            </a>
                         </div>
                         <div className='footer-icons-group'>
-                            <div className='footer-icon-container'>
-                                <Icon link size={'large'}  name='twitter' />
-                            </div>
-                            <div className='footer-icon-container'>
-                                <Icon link size={'large'}  name='linkedin' />
-                            </div>
-                            <div className='footer-icon-container'>
-                                <Icon link size={'large'}  name='facebook f' />
-                            </div>
-                            <div className='footer-icon-container'>
-                                <Icon link size={'large'}  name='vk' />
-                            </div>
+                            {
+                                social.map((item) => {
+                                    return (<div className='footer-icon-container'>
+                                        <Icon link size={'large'} name={item}/>
+                                    </div>)
+                                })
+                            }
                         </div>
                         <div className="footer-right-link-container">
                             <Dropdown text='Помощь' closeOnChange>
                                 <Dropdown.Menu>
-                                    <Dropdown.Item text='Помощь' description='Помощь' />
+                                    <Dropdown.Item text='Помощь' description='Помощь'/>
                                 </Dropdown.Menu>
                             </Dropdown>
                         </div>
-                        <ScrollUpButton style={{bottom: 50, backgroundColor: 'none', border: 'none'}} EasingType="easeOutCubic"/>
+                        <ScrollUpButton style={styles.ScrollUpButton} EasingType="easeOutCubic"/>
                     </div>
                 ) : (
-                    <div className="footer">
-
-                    </div>
+                    <div className="footer"/>
                 )}
             </div>
         )
     }
 }
+
+AppFooter.propTypes = {
+    authenticated: PropTypes.bool.isRequired,
+};
 
 export default AppFooter;
