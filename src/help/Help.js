a0d21c34e9f1d08367bded8b12028a5fa6d5a2d2
Azat Akhunov <azat.akhunov@protonmail.com>
2020-04-06 13:52:48 +0300

    [CF] help page extended

--- src/help/Help.js
+++ src/help/Help.js
@@ -50,9 +50,34 @@ class Help extends Component {
                 <div className="help-body-container">
                     <div className="help-inner-body-container">
                         <div className="help-inner-left-container">
-                            <span>Сейчас мы находимся в стадии разработки,</span><br/>
-                            <span>поэтому информация будет пополняться</span><br/>
-                            <span>по мере реализации :)</span><br/>
+                            <div className='help-body-element'>
+                                <span className='help-header-label'>FAQ</span>
+                                <span className='help-header-body'>Здесь мы соберем самые распространенные вопросы о продукте</span><br/>
+                            </div>
+                            <div className='help-body-element help-body-element-padded'>
+                                <span className='help-header-label'>Магазин API</span>
+                                <span className='help-header-body-links'>О Магазине</span>
+                                <span className='help-header-body-links'>Настройка фильтрации</span>
+                                <span className='help-header-body-links'>Подключение API</span>
+                            </div>
+                            <div className='help-body-element help-body-element-padded-second'>
+                                <span className='help-header-label'>Личный кабинет</span>
+                                <span className='help-header-body-links'>Настройка профиля</span>
+                                <span className='help-header-body-links'>Загрузка API</span>
+                                <span className='help-header-body-links'>Администрирование</span>
+                            </div>
+                        </div>
+                        <div className="help-inner-right-container">
+                            <div className='help-body-element'>
+                                <span className='help-header-label'>Учетная запись</span>
+                                <span className='help-header-body-links'>Регистрация</span>
+                                <span className='help-header-body-links'>Авторизация и выход</span>
+                            </div>
+                            <div className='help-body-element help-body-element-padded'>
+                                <span className='help-header-label'>Интеграторы</span>
+                                <span className='help-header-body-links'>Об интеграторах</span>
+                                <span className='help-header-body-links'>Работа с интеграторами</span>
+                            </div>
                         </div>
                     </div>
                 </div>
