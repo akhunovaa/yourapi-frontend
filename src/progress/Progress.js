d05a1cd36e9e8720e199e8aa27c6fbdacd75da28
Nikolai ADIUTANTOV <nikolai.adiutantov@yandex.ru>
2020-04-06 15:10:03 +0300

    [CF] Progress component refactoring

--- src/progress/Progress.js
+++ src/progress/Progress.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Progress.css";
+import * as PropTypes from "prop-types";
 
 class Progress extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
     }
+
     render() {
+        const {progress} = this.props;
+        const style = {
+            progress: {
+                width: progress + "%"
+            }
+        };
         return (
             <div className="ProgressBar">
-                <div
-                    className="Progress"
-                    style={{ width: this.props.progress + "%" }}
-                />
+                <div className="Progress" style={style.progress}/>
             </div>
         );
     }
 }
+
+Progress.propTypes = {
+    progress: PropTypes.any.isRequired
+};
 
 export default Progress;
